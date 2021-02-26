dd31734326c5ca14fd9eeecefdd5c28d34fa2340
Jackson Machado <jackson.machado@ngi.com.br>
2021-02-25 22:14:59 -0300

    feat: create customer processes

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace csharp_vehicles
+{
+    public class Program
+    {
+        /// <summary>
+        /// Start the system
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            int opt = 0;
+            Console.WriteLine("Bem vindo a nossa revenda");
+            // Always repeat until the user leaves
+            do {
+                Console.WriteLine("Digite a operação de Menu");
+                Console.WriteLine("1 - Cadastrar Cliente");
+                Console.WriteLine("2 - Lista de Clientes");
+                Console.WriteLine("0 - Sair");
+                // Get the user option
+                opt = Convert.ToInt32(Console.ReadLine());
+                switch(opt) {
+                    case 0:
+                        // Close system
+                        break;
+                    case 1:
+                        View.Customer.CreateCustomer();
+                        break;
+                    case 2:
+                        View.Customer.ListCustomers();
+                        break;
+                    default:
+                        Console.WriteLine("Operação Inválida.");
+                        break;
+                }
+            } while(opt != 0);
+        }
+
+    }
+}
